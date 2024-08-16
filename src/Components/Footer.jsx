2fdb6cd289631839273a8e3ac1e7659a4c0b66c2
Paulmat33 <paultobi33@gmail.com>
2024-08-16 12:38:07 +0100

    second large screen

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,14 +7,12 @@ import instagram from "../assets/instagram.png";
 
 const Footer = () => {
   return (
-    <footer className="bg-black mt-[96px] px-[56px] text-white lg:flex lg:flex-row lg:items-center l ">
-
-
-      <div className="lg:pl-[120px]">
-        <div>
+    <footer className="bg-black mt-[96px] px-[56px] text-white lg:flex lg:justify-between lg:items-center lg:pr-[170px] lg:pb-11  ">
+      <div className="pt-14 flex flex-col items-center lg:pl-[120px] lg:pt-[44px] lg:flex lg:flex-col lg:items-start lg:gap-4  ">
+        <div className="pb-8 lg:pb-0 ">
           <img src={Logo} alt="logo" />
         </div>
-        <div className="lg:flex lg:flex-row lg:gap-4">
+        <div className="flex flex-col text-center gap-4 pb-10 lg:flex lg:flex-row lg:gap-4">
           <p>About</p>
           <p>Careers</p>
           <p>Event</p>
@@ -23,19 +21,24 @@ const Footer = () => {
         </div>
       </div>
 
-
-
-      <div className="lg:pr-[140px]">
-        <div className="lg:flex lg:flex-row lg:justify-items-end lg:gap-4 lg:mr-">
-          <img src={facebook} alt="facebook" />
-          <img src={twitter} alt="twitter" />
-
-          <img src={pinterest} alt="pinterest" />
-          <img src={instagram} alt="instagram" />
+      <div className=" flex flex-col items-center  lg:flex lg:flex-col lg:items-end ">
+        <div className="flex flex-row gap-4 pb-4 lg:flex lg:flex-row lg:justify-items-end  lg:pb-6">
+          <div>
+            <img src={facebook} alt="facebook" />
+          </div>
+          <div>
+            <img src={twitter} alt="twitter" />
+          </div>
+          <div>
+            <img src={pinterest} alt="pinterest" />
+          </div>
+          <div>
+            <img src={instagram} alt="instagram" />
+          </div>
         </div>
 
         <div>
-          <p>© 2021 Loopstudios. All rights reserved.</p>
+          <p className="font-alata text-white font-normal leading-[25px] opacity-50 text-center xs:text-[11px] sm:text-[15px]">© 2021 Loopstudios. All rights reserved.</p>
         </div>
       </div>
     </footer>
--- src/Components/InteractiveVr.jsx
+++ src/Components/InteractiveVr.jsx
@@ -11,7 +11,7 @@ const InteractiveVr = () => {
       <div className='mt-[48px] md:mt-[40px] lg:max-w-[541px] lg:absolute lg:right-0 lg:left-[750px] lg:top-[905px]  '>
         <div className='lg:pt-[96px] lg:pl-[96px] lg:bg-white '>
           <h1 className='font-josefinSans font-light leading-[32px] uppercase text-black text-[32px] text-center md:text-[35px] lg:text-[48px] lg:leading-[48px] lg:w-[445px] lg:text-left'>The leader in interactive VR</h1>
-          <p className='mt-[16px] font-alata font-normal leading[25px] text-black text-[15px] text-center opacity-50 md:text-[20px] lg:text-[15px] lg:text-left lg:w-[445px] '>Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital experiences that bind to their brand.</p>
+          <p className='mt-[16px] font-alata font-normal leading[25px] text-black text-[15px] text-center opacity-50 md:text-[20px] lg:text-[15px] lg:text-left lg:w-[425px] '>Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital experiences that bind to their brand.</p>
         </div>
       </div>      
     </section>
